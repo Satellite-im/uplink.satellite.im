f6597d891fbc194d6294468d9f710ae7f7d203a5
stavares843 <stavares843@users.noreply.github.com>
2024-03-27 22:14:36 +0000

    deploy: e64b4d95453c331252ee0188ea1a727767fa6ac8

--- puppeteer-check.js
+++ puppeteer-check.js
@@ -1,16 +1,30 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
+const checkPagesStatus = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://github.com/Satellite-im/uplink.satellite.im/settings/pages');
+  
+  let isLive = false;
 
-  const isLive = await page.evaluate(() => {
-    const liveElement = document.querySelector('div.Box-row.d-flex.flex-items-center');
-    return liveElement && liveElement.innerText.includes('Your site is live');
-  });
+  while (!isLive) {
+    await page.goto('https://github.com/Satellite-im/uplink.satellite.im/settings/pages');
+
+    isLive = await page.evaluate(() => {
+      const liveElement = document.querySelector('div.Box-row.d-flex.flex-items-center');
+      return liveElement && liveElement.innerText.includes('Your site is live');
+    });
+
+    if (!isLive) {
+      // Wait for 10 seconds before checking again
+      await new Promise(resolve => setTimeout(resolve, 10000));
+    }
+  }
 
   await browser.close();
-  
+
   return isLive;
-})();
+};
+
+checkPagesStatus().then((isLive) => {
+  console.log('GitHub Pages status:', isLive ? 'Live' : 'Not live');
+});
